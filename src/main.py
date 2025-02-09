ace11c4056921043377b136a7501808974d6830a
jusluz <julioluz84@yahoo.com>
2025-02-09 02:22:31 -0300

    extract funcionando

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
 from pipeline.extract import extract_from_excel
 
-lista_de_dataframe = extract_from_excel(path='data/input')
+lista_de_dataframe = extract_from_excel("data/input")
 print(lista_de_dataframe)
--- src/pipeline/extract.py
+++ src/pipeline/extract.py
@@ -3,6 +3,7 @@ import glob # Bilioteca para listar arquivos
 
 import pandas as pd
 from typing import List
+import openpyxl
 
 '''
 Função para ler arquivos da pasta data/input e retornar uma lista de Data Frames
@@ -11,8 +12,6 @@ args: input_path (str): caminho da pasta com os arquivos
 
 return lista de Data frames
 '''
-path = 'data/input'
-
 def extract_from_excel(path: str) -> List[pd.DataFrame]:
     # Listar todos os arquivos .xlsx na pasta
     all_files = glob.glob(os.path.join(path, "*.xlsx"))
@@ -20,10 +19,13 @@ def extract_from_excel(path: str) -> List[pd.DataFrame]:
     data_frame_list = []
     for file in all_files:
         # Ler o arquivo .xlsx e converter em Data Frame
-        data_frame_list.append(pd.read_excel(file))
+        data = pd.read_excel(file)
+        data_frame_list.append(data)
         
     return data_frame_list
 
 if __name__ == "__main__":
-    data_frame_list = extract_from_excel(path)
+    data_frame_list = extract_from_excel(path="/data/input")
     print(data_frame_list)
+    
+    
